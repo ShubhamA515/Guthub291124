708e8c137a59c5e7314e770b0b90245322585770
Shubham GMAIL <agrawals8242@gmail.com>
2024-11-30 10:03:38 +0530

    commit 3011

---
+++ src/sample/test1.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class test1 
+{
+public static void main(String[] args) {
+	System.out.println("Test1 on 30.11.24");
+	
+	System.out.println("Test1 updated on 30.11.24--> sending to remote using CMD ");
+	
+	
+}
+}
